5ba5c5db23adae291e564c0b3c6f2d640e045096
vnurhaqiqi <viqinurhqiqi@gmail.com>
2021-11-03 20:54:23 +0700

    add orders model & finish orders services

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,76 +1,118 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Order = require("../models/orders");
+const Product = require("../models/products");
 
 const route = express.Router();
 
 // == GET ALL ORDERS ==
 route.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "GET orders"
-    });
+    Order.find()
+        .populate("product", "name price")
+        .exec()
+        .then(result => {
+            const response = {
+                count: result.length,
+                data: result.map(order => {
+                    return {
+                        _id: order._id,
+                        qty: order.qty,
+                        product: order.product,
+                        request: {
+                            type: "GET",
+                            url: `${req.protocol}://${req.get('host')}${req.originalUrl}${order._id}`
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 // == CREATE ORDER ==
 route.post("/", (req, res, next) => {
-    const orderPayload = {
-        productId: req.body.productId,
-        qty: req.body.qty
-    }
-
-    res.status(201).json({
-        message: "order has been created",
-        order: orderPayload
-    });
+    Product.findById(req.body.productId)
+        .then(product => {
+            if (!product) {
+                return res.status(500).json({
+                    message: "product not found",
+                });
+            } else {
+                const order = new Order({
+                    _id: mongoose.Types.ObjectId(),
+                    product: product._id,
+                    qty: req.body.qty
+                });
+                return order.save();
+            }
+        }).then(result => {
+            const response = {
+                message: "order has been created",
+                data: {
+                    _id: result._id,
+                    qty: result.qty,
+                    product: result.product,
+                    request: {
+                        type: "GET",
+                        url: `${req.protocol}://${req.get('host')}${req.originalUrl}${result._id}`
+                    }
+                }
+            }
+            res.status(201).json(response);
+        }).catch(err => {
+            res.status(500).json({ error: err });
+        });
 });
 
 // == GET DETAILS ORDER ==
 route.get("/:orderId", (req, res, next) => {
     const orderId = req.params.orderId;
 
-    if (orderId) {
-        res.status(200).json({
-            message: `GET order id ${orderId}`,
-            id: orderId
+    Order.findById(orderId)
+        .populate("product", "name price")
+        .exec()
+        .then(result => {
+            if (!result) {
+                return res.status(404).json({
+                    message: "order not found",
+                });
+            } else {
+                const response = {
+                    data: {
+                        _id: result._id,
+                        qty: result.qty,
+                        product: result.product,
+                        request: {
+                            type: "GET",
+                            url: `${req.protocol}://${req.get('host')}${req.originalUrl}/`
+                        }
+                    }
+                }
+                res.status(200).json(response);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
         });
-    } else {
-        res.status(404).json({
-            message: `order id ${orderId} not found`,
-            id: orderId
-        });
-    }
-});
-
-// == UPDATE ORDER ==
-route.patch("/:orderId", (req, res, next) => {
-    const orderId = req.params.orderId;
-
-    if (orderId) {
-        res.status(200).json({
-            message: `UPDATE order id ${orderId}`,
-            id: orderId
-        });
-    } else {
-        res.status(404).json({
-            message: `order id ${orderId} not found`,
-            id: orderId
-        });
-    }
 });
 
 // == DELETE ORDER ==
 route.delete("/:orderId", (req, res, next) => {
     const orderId = req.params.orderId;
 
-    if (orderId) {
-        res.status(200).json({
-            message: `DELETE order id ${orderId}`,
-            id: orderId
+    Order.remove({ _id: orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({ message: `order with ID ${orderId} has been deleted` });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
         });
-    } else {
-        res.status(404).json({
-            message: `order id ${orderId} not found`,
-            id: orderId
-        });
-    }
 });
 
 module.exports = route;
